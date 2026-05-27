96fca8776dffe7c5e7a2188ac0890594524f6868
Zoe <zleullie@student.42sp.org.br>
2026-05-27 11:50:59 -0300

    Refactor ft_calloc to improve error handling and update timestamp in header

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,21 +6,25 @@
 /*   By: zleullie <zleullie@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/15 19:56:01 by zleullie          #+#    #+#             */
-/*   Updated: 2026/05/15 22:04:58 by zleullie         ###   ########.fr       */
+/*   Updated: 2026/05/27 11:43:01 by zleullie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <limits.h>
 #include "libft.h"
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*array;
 
-	array = malloc(nmemb * size);
-	if (array == NULL)
+	if (!nmemb || !size)
+		return (malloc(0));
+	if (nmemb > INT_MAX / size)
 		return (NULL);
-	ft_bzero(array, nmemb * size);
+	array = malloc(nmemb * size);
+	if (array != NULL)
+		ft_bzero(array, nmemb * size);
 	return (array);
 }
